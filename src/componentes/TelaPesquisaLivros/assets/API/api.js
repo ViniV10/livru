5f2a81d328d05a792e57185458d075faba8766df
Vinícius <viniciusvsb0@gmail.com>
2021-07-05 21:18:56 -0300

    Remove api

--- src/componentes/TelaPesquisaLivros/assets/API/api.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = 'AIzaSyDMvbOFgThqJ2TV48w7SXPWeAS6u7t0osc';
-export const googleBooksURL = 'https://www.googleapis.com/books/v1/volumes?q=';
-export const keyParaPesquisa = '&key=' + API_KEY;
